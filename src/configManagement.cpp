9120cf25fbbb0265286df94d889346688845f563
ahickling2004 <ianhickling0@gmail.com>
2025-05-28 19:00:02 +0000

    added toml++

---
+++ src/configManagement.cpp
@@ -0,0 +1,12 @@
+#include <toml++/toml.h>
+#include <iostream>
+
+int mkDefault(){
+	/*This function will generate default configs in a .toml fileso that the program can
+	 *use those defaults when loading the program first.
+
+	 as soon as I figure out the lib*/
+
+	return 0;
+}
+
